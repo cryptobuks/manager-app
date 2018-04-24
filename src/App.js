8177d3b7a486773fcd5367c07b11a47b7115e791
Nam Phung <nphung@macalester.edu>
2018-04-23 23:27:40 -0500

    Set up boilerplate for app and installed dependencies

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyAHcLnaowrGuEmKV-yQn4Hv6-Bpe_1ouz8",
+      authDomain: "manager-53046.firebaseapp.com",
+      databaseURL: "https://manager-53046.firebaseio.com",
+      projectId: "manager-53046",
+      storageBucket: "",
+      messagingSenderId: "614185581689"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
