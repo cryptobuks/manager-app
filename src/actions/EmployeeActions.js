c747251d2bbcfc31bfdad6e7e3f844a7a87e3f3f
Nam Phung <nphung@macalester.edu>
2018-04-29 02:35:52 -0500

    Set up firebase database to store user data

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,10 +1,20 @@
+import firebase from 'firebase';
 import {
-  EMPLOYEE_UPDATE
-} from './types'
+  EMPLOYEE_UPDATE,
+} from './types';
+
 
 export function employeeUpdate({ prop, value }) {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
   };
+}
+
+export function employeeCreate({ name, phone, shift }) {
+  const { currentUser } = firebase.auth();
+
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
+
 }
