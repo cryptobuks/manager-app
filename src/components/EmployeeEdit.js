2a468a3f71c07eb2ce5fa8430e7de2af2246e48c
Nam Phung <nphung@macalester.edu>
2018-05-11 11:46:19 -0500

    Created separate component to show and edit employees

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Button } from './common';
+
+class EmployeeEdit extends Component {
+  componentWillMount() {
+    _.each(this.props.employee, (value, prop) => { //iterate over all property of object
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+
+    console.log(name, phone, shift);
+  }
+
+  render() {
+    return (
+      <Card>
+        <EmployeeForm />
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Save Changes
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
